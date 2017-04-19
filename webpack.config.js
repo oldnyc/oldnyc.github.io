99d9e9b495a0f513ab53b22d7487aba1999b7492
Dan Vanderkam <danvk@sidewalklabs.com>
2017-04-19 19:31:39 -0400

    Drop devtool

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     main: './js/entry.js',
     ocr: './js/ocr-tool.js'
   },
-  devtool: '#cheap-module-source-map',
+  // devtool: '#cheap-module-source-map',
   output: {
     path: __dirname + '/js/bundle',
     filename: '[name].js'
