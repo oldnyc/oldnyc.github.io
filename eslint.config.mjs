c1d3ebf9d818705420ae2cd574043ffa6b070f7c
Dan Vanderkam <danvdk@gmail.com>
2024-08-19 15:12:55 -0400

    try to enable recommendedTypeChecked

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,36 @@
 // @ts-check
 
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+
+  // See https://stackoverflow.com/a/64488474/388951
+  ...tseslint.configs.recommendedTypeChecked.map((config) => ({
+    ...config,
+    files: ['src/**/*.ts'], // We use TS config only for TS files
+  })),
+  {
+    files: ['**/*.ts'],
+
+    // This is required, see the docs
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname
+      },
+    },
+
+    // This is needed in order to specify the desired behavior for its rules
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+
+    // After defining the plugin, you can use the rules like this
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'error',
+    }
+  }
 );
