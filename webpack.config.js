b5dff9e0a8fd20af3a5fce4b579585b142f182e2
Dan Vanderkam <danvdk@gmail.com>
2024-08-19 12:27:31 -0400

    Move live TS into src dir, set up webpack chunking

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 /*eslint-env node */
 module.exports = {
   mode: 'development',
   entry: {
-    main: './js/main.tsx',
-    ocr: './js/ocr-tool.ts'
+    main: './src/main.tsx',
+    // ocr: './js/ocr-tool.ts'
   },
   // devtool: '#cheap-module-source-map',
   output: {
-    path: __dirname + '/js/bundle',
-    filename: '[name].js'
+    clean: true,
+    path: __dirname + '/dist/js',
+    filename: '[name].[contenthash].js'
   },
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
@@ -36,5 +39,12 @@ module.exports = {
     // require("jquery") is external and available
     //  on the global var jQuery
     "jquery": "jQuery"
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.template.html',
+      filename: __dirname + '/dist/index.html',
+      chunks: ["main"]
+    })
+  ]
 };
