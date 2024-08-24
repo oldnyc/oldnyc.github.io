a94cd330a3a2a8b5463dea0cb3ac2e9f6c3d611f
Dan Vanderkam <danvdk@gmail.com>
2024-08-24 17:37:04 -0400

    Expand date pattern a bit

--- static/timestamps.js
+++ static/timestamps.js
@@ -2,5 +2,5 @@ var timestamps = {
   "ocr_ms": 1724265118707,
   "ocr_time": "2024-08-21T14:31:58",
   "rotation_time": "2024-08-14T15:09:02",
-  "timestamp": "2024-08-24T21:24:11Z"
+  "timestamp": "2024-08-24T21:36:14Z"
 };
