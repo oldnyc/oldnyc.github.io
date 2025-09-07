fcf70cce13ecbe4f2c2a89ddeaab0050486755c2
Dan Vanderkam <danvdk@gmail.com>
2025-09-07 09:57:43 -0400

    tile downloader

--- osm_tiles.py
+++ osm_tiles.py
@@ -14,10 +14,10 @@ def deg2num(pos, zoom):
 if __name__ == '__main__':
 
     sw, ne = (-74.25423,40.500426), (-73.707672,40.912507)
-    bucket = 's3://long-term.cache.maps.stamen.com/toner-lite'
+    base = 'https://vector.openstreetmap.org/shortbread_v1'
 
     count = 0
-    for z in range(10, 17):
+    for z in range(10, 15):
         x0, y0 = deg2num(sw, z)
         x1, y1 = deg2num(ne, z)
         n = (abs(x1 - x0) + 1) * (abs(y1 - y0) + 1)
@@ -25,15 +25,10 @@ if __name__ == '__main__':
         count += n
         for x in range(min(x0, x1), 1 + max(x0, x1)):
             for y in range(min(y0, y1), 1 + max(y0, y1)):
-                for r in ('', '@2x'):
-                # for r in ('',):
-                    url = f'{bucket}/{z}/{x}/{y}{r}.png'
-                    out = f'static/toner-lite/{z}-{x}-{y}{r}.png'
-                    if not os.path.exists(out):
-                        if r == '@2x':
-                            out_chunky = f'static/toner-lite/{z}-{x}-{y}.png'
-                            if os.path.exists(out_chunky) and os.path.getsize(out_chunky) < 900:
-                                sys.stderr.write(f'dropping {out}\n')
-                        print(f's3cmd get {url} {out} --requester-pays')
+                url = f'{base}/{z}/{x}/{y}.mvt'
+                out = f'static/tiles/{z}-{x}-{y}.mvt'
+                if not os.path.exists(out):
+                    print(f'curl --output {out} {url}')
+                    print('sleep 10')
 
     # print(f'total: {n}')
