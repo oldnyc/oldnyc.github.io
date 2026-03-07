05578b2b9c54b06febff9cf876f16247a2888982
Dan Vanderkam <danvdk@gmail.com>
2026-03-07 10:08:51 -0500

    remove social links

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,29 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Like } from './facebook';
 
 export function Header() {
   return (
     <div className="social-about">
-      <div id="facebook" className="hide-mobile">
-        <Like
-          // https would be better, but the like count is much lower.
-          url="http://www.oldnyc.org"
-          layout="button_count"
-          id="header-like"
-        />
-      </div>
-      <div id="twitter" className="hide-mobile">
-        <a
-          href="http://twitter.com/share"
-          className="twitter-share-button"
-          data-count="horizontal"
-          data-via="Old_NYC @NYPL"
-        >
-          Tweet
-        </a>
-      </div>
-
       <div id="about">
         <Link to="/about">About</Link>
       </div>
